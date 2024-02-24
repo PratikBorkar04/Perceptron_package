54638a920a3e720d83138a9c8f0a1be4dc3a140a
Pratik Borkar <pratikab01@gmail.com>
2024-02-24 13:52:16 +0530

    and file created 13.52/24.02.24

--- and.py
+++ and.py
@@ -1,5 +1,8 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data,save_model,save_plot
+import pandas as pd
+import numpy as np
+
 AND = {
     "x1" : [0,0,1,1],
     "x2" : [0,1,0,1],
@@ -17,3 +20,5 @@ EPOCHS = 10
 model = Perceptron(eta=ETA,epochs= EPOCHS)
 model.fit(X,y)
 __ = model.total_loss() 
+save_model(model,filename = "and.model")
+save_plot(df,"and.png",model)
